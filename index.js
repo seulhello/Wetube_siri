4a04686f4c41bce7261ebf1dedb439b519550b4a
siri_06 <siri_06@rnd.skinfosec.com>
2018-12-10 15:39:53 +0900

    1차 노드 수업

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("hi");
+import express from "express";
+import morgan from "morgan";
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () => console.log(`Listening on : http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send('hello from test');
+
+const handleProfile = (req, res) => res.send("your proflie");
+
+const betweenHome = (req, res, next) => {
+  console.log("test");
+  next();
+};
+
+app.use(betweenHome); //global 실행 
+
+app.get("/", handleHome); // 사이에 함수를 넣으면 해당 부분의 url 만 실행된다.
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
