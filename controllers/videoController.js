5f541c654ff9d65ad7a2f8f428115e35bd5ca332
siri_06 <siri_06@rnd.skinfosec.com>
2019-01-02 17:30:31 +0900

    pug 3차 커밋

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,13 +1,14 @@
-export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+import { videos } from "../db";
+export const home = (req, res) => {
+  res.render("home", { pageTitle: "Home", videos });
+};
 export const search = (req, res) => {
   const {
     query: { term: searchingBy }
   } = req;
   res.render("Search", { pageTitle: "Search", searchingBy: searchingBy });
 }
-
-export const videos = (req, res) => res.render("Videos", { pageTitle: "Videos" });
-export const upload = (req, res) => res.render("Upload", { pageTitle: "Upload" });
-export const videoDetail = (req, res) => res.render("Video Detail", { pageTitle: "Video Detail" });
-export const editVideo = (req, res) => res.render("Edit Video", { pageTitle: "Edit Video" });
-export const deleteVideo = (req, res) => res.render("Delete Video", { pageTitle: "Delete Video" });
+export const upload = (req, res) => res.render("upload", { pageTitle: "Upload" });
+export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });
+export const editVideo = (req, res) => res.render("editVideo", { pageTitle: "Edit Video" });
+export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Video" });
