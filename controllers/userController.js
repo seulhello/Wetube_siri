d179921271bb84c3d5cfd015142986143b3b26d9
siri_06 <siri_06@rnd.skinfosec.com>
2019-01-07 13:44:19 +0900

    DB채우기 이전 백그라운드 코딩

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,8 +17,15 @@ export const postJoin = (req, res) => {
   }
 }
 
-export const login = (req, res) => res.render("Login", { pageTitle: "Login" });
-export const logout = (req, res) => res.render("Logout", { pageTitle: "Logout" });
+export const getLogin = (req, res) => res.render("Login", { pageTitle: "Login" });
+export const postLogin = (req, res) => {
+  res.redirect(routes.home);
+}
+
+export const logout = (req, res) => {
+  //ToDo: 로그아웃 처리
+  res.redirect(routes.home);
+}
 export const users = (req, res) => res.render("Users", { pageTitle: "Users" });
 export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Detail" });
 export const editProfile = (req, res) => res.render("editProfile", { pageTitle: "Edit Profile" });
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,6 @@
 import { videos } from "../db";
+import routes from "../routes";
+
 export const home = (req, res) => {
   res.render("home", { pageTitle: "Home", videos });
 };
@@ -8,7 +10,16 @@ export const search = (req, res) => {
   } = req;
   res.render("Search", { pageTitle: "Search", searchingBy: searchingBy, videos });
 }
-export const upload = (req, res) => res.render("upload", { pageTitle: "Upload" });
+export const getUpload = (req, res) => {
+  res.render("upload", { pageTitle: "Upload" });
+}
+export const postUpload = (req, res) => {
+  const {
+    body: { file, title, description }
+  } = req;
+  // To Do: 비디오 업로드 및 세이브
+  res.redirect(routes.videoDetail(324393));
+};
 export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });
 export const editVideo = (req, res) => res.render("editVideo", { pageTitle: "Edit Video" });
 export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Video" });
